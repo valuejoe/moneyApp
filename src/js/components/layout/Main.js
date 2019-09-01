575bfe023ee2a5b811e0fe1c80aff0840d4b6320
valuejoe <chiaohung82@gmail.com>
2019-09-01 20:48:48 +0800

    delete old Sigin.js

--- src/js/components/layout/Main.js
+++ src/js/components/layout/Main.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Layout from './Layout'
-import SignIn from '../auth/SignIn'
 import SignUp from '../auth/SignUp'
 import Books from '../dashboard/Books'
 import Statistics from '../dashboard/Statistics'
